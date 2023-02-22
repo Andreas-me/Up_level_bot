19c5e0da9d606b6c3b8e76d92c6d89bbdbdd5925
Andreas-me <andreas.surname.me@gmail.com>
2023-02-22 14:49:32 +0200

    Version 1.0

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import telebot  # 4.9.0 version library
 from telebot import types
+import os
 
-bot = telebot.TeleBot('6217176905:AAHXOO-00y4qzE7qLo8smdZMOEYhWrtR-VQ', parse_mode='html')
+bot = telebot.TeleBot('', parse_mode='html')
 
 
-def up_data(a_level):
-    with open('level.txt', 'wb'):  # clean file
+def up_data(a_level, cid):
+    with open(f'data_user/{cid}.txt', 'wb'):  # clean file
         pass
 
-    f = open('level.txt', 'w')
+    f = open(f'data_user/{cid}.txt', 'w')
     f.write(str(a_level))
+    f.close()
+
+
+def up_data_2(cid):  # clean file and write 0
+    with open(f'data_user/{cid}.txt', 'wb'):  # clean file
+        pass
+
+    f = open(f'data_user/{cid}.txt', 'w')
+    f.write(str('0'))
     f.close()
 
 
@@ -25,27 +35,42 @@ def welcome(m):
                          m.from_user, bot.get_me()), reply_markup=markup)
 
 
-# photo1 = open(r"C:\Users\Andreas\Documents\Adobe\Photoshop\level up\new.png", 'rb')
-#
-#
-# @bot.message_handler(commands=['photo'])
-# def msg4(message):
-#     bot.send_photo(message.chat.id, photo1)
-
 @bot.message_handler(content_types=['text'])
 def main(m):
+    cid = m.chat.id  # get user info
+    filename = f"{cid}.txt"  # name file
+    directory = "data_user"  # directory file
     if m.chat.type == 'private':
         if m.text == 'Up level':
 
+            if os.path.isfile(os.path.join(directory, filename)):  # if user register
+                pass
+
+            else:
+                new_file = open(f'data_user/{cid}.txt', 'w')
+                new_file.write('0')
+                new_file.close()
+
             the_level = []
-            with open('level.txt') as f:
+            with open(f'data_user/{cid}.txt') as f:  # write data to variable
                 for line in f:
                     the_level.extend([int(x) for x in line.split()])
-
             the_level = the_level[0] + 1
 
-            up_data(the_level)
-            bot.send_message(m.chat.id, f'Your new level {the_level}!')
+            if the_level < 100:
+                up_data(the_level, cid)
+                photo = open(f'level_img/{the_level}.png', 'rb')
+                bot.send_photo(m.chat.id, photo)
+                bot.send_message(m.chat.id, f'Your next level {the_level}!')
+
+            else:
+                up_data(the_level, cid)
+                photo = open(f'level_img/{the_level}.png', 'rb')
+                bot.send_photo(m.chat.id, photo)
+                bot.send_message(m.chat.id,
+                                 f'Your next level {the_level}!\nYou’ve reached the maximum level!\nCongratulations again, everyone! ')
+                up_data_2(cid)
+                bot.send_message(m.chat.id, 'Level of anylation')
 
         else:
             bot.send_message(m.chat.id, 'I can only level up')
